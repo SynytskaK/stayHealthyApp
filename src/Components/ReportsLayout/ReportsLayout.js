aedfdc0aa3311d2a2cd6d2a62e1d784919bb68c9
“SynytskaK” <pankiv38@gmail.com>
2025-05-12 17:20:58 -0400

    review2

--- src/Components/ReportsLayout/ReportsLayout.js
+++ src/Components/ReportsLayout/ReportsLayout.js
@@ -3,7 +3,7 @@ import './ReportsLayout.css'
 const ReportsLayout = () => {
     const appointments = JSON.parse(sessionStorage.getItem("appointment"));
 
-    return(
+    return (
         <div className="wrapper-review">
             <h1>Reports</h1>
             <div className='category-review'>
@@ -15,19 +15,32 @@ const ReportsLayout = () => {
                     <li>Downoload Report</li>
                 </ul>
 
-                {appointments && appointments.map((item, index) => (
+                {appointments ? appointments.map((item, index) => (
                     <ul className='review' key={index}>
                         <li>{index + 1}</li>
                         <li>{item.doctorName}</li>
                         <li>{item.doctorSpeciality}</li>
                         <li>
-                           <button>View Report</button>
+                            <a
+                            target='_blank'
+                            rel="noopener noreferrer"
+                            className='anchor'
+                            href="/fake_medical_report.pdf"
+                            >
+                                View Report
+                            </a>
                         </li>
                         <li>
-                           <button>Downoload Report</button>
+                            <a
+                            className='anchor'
+                                href="/fake_medical_report.pdf"
+                                download="Medical_Report.pdf"
+                            >
+                                Download Report
+                            </a>
                         </li>
                     </ul>
-                ))}
+                )) : <p>No Reports yet</p>}
                 <div className='divider'></div>
             </div>
         </div>
